4eca08f2695b004f0608eb178852e7addf557012
Mykhailo Rud <rudmm@ukr.net>
2019-10-18 21:38:00 +0300

    menu mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,16 +2,25 @@ function viewMenu(){
     let menu = $('.header .navbar');
     let scroll = $(window).scrollTop();
     if(scroll>0){
-        menu.addClass('open');
+        menu.addClass('view');
     }else{
-        menu.removeClass('open');
+        menu.removeClass('view');
     }
 }
 
+function openMobileMenu(){
+    let btn = $('.header .navbar .btn');
+    let mobile = $('.header .navbar .mobile');
+    btn.click(function () {
+        btn.toggleClass('open');
+        mobile.slideToggle();
+    });
+}
+
 $(window).scroll(function () {
-    //viewMenu();
+    viewMenu();
 });
 
 $(document).ready(function () {
-
+    openMobileMenu();
 });
